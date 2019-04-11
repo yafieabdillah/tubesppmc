e4fac680c378e66de527e99eaf3bcecf8e28f4eb
lisasim <49404284+lisasim@users.noreply.github.com>
2019-04-11 11:57:04 +0700

    Update muat_proyek.c

--- muat_proyek.c
+++ muat_proyek.c
@@ -1,56 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-void cekmode(int n){
-    while ((n>=9)||(n<=0)){
-        printf("Masukan salah\n");
-        printf("Pilih Mode ");
-        scanf("%d", &n);
-    }
-    return n;
-}
-
-void tampilkanlayout(char tabs[][40][4], int panjang, int lebar){
-    int i,j;
-
-    for (i=1; i<=panjang; i++){
-        printf("\t %d", i);
-    }
-    printf("\n");
-    for (i=1;i<=lebar;i++){
-        printf("%d", i);
-        for (j=1; j<=panjang; j++){
-            printf("\t %s", tabs[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void tampilkanrouting(char tabs[][40], int panjang, int lebar){
-    int i, j;
-
-    for (i=1; i<=panjang; i++){
-        printf("\t %d", i);
-    }
-    printf("\n");
-    for (i=1; i<=lebar;i++){
-        printf("%d", i);
-        for (j=1; j<=panjang; j++){
-            printf("\t %c", tabs[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int main(){
+void muat_proyek(){
     FILE *file1;
     FILE *file2;
     char namafile[100],flayout[100],frouting[100];
-    int panjang, lebar, mode, i, j, k, c, d, count, hitung;
+    int panjang, lebar, mode, i, j, k, c, count, hitung;
+    char d;
     char tabslay[40][40][4];
     char tabsrout[40][40];
-    char a[100];
     char data[50];
 
     printf("====== Memuat Proyek dari Berkas ======\n");
@@ -60,7 +15,7 @@ int main(){
     strcat(flayout, "_layout.csv");
     strcpy(frouting, namafile);
     strcat(frouting, "_routing.csv");
-    printf("%s %s \n",flayout, frouting);
+    //printf("%s %s \n",flayout, frouting);
 
     while(((file1=fopen(flayout,"r"))==NULL)||((file2=fopen(frouting,"r"))==NULL)){
         printf("file tidak ada\n");
@@ -96,11 +51,11 @@ int main(){
                 k=0;
                 j=1;
                 i=i+1;
-                printf("\n");
+                //printf("\n");
                 break;
             case ' ':
                 tabslay[i][j][1]=d;
-                printf("/", tabslay[i][j][1]);
+                //printf("/", tabslay[i][j][1]);
                 strcpy(tabslay[i][j], "  ");
                 break;
             case 'R':
@@ -108,7 +63,7 @@ int main(){
             case 'T':
             case 'C':
                 tabslay[i][j][k]=d;
-                printf("%c", tabslay[i][j][k]);
+                //printf("%c", tabslay[i][j][k]);
                 k=k+1;
                 break;
             case '0' :
@@ -122,23 +77,21 @@ int main(){
             case '8' :
             case '9' :
                 tabslay[i][j][k]=d;
-                printf("%c", tabslay[i][j][k]);
+                //printf("%c", tabslay[i][j][k]);
                 k=k+1;
                 break;
-            default :
-                printf("/%d/", d);
+            //default :
+                //printf("/%d/", d);
         }
         if(feof(file1)){
             break;
         }
     }
 
-    //cobacoba
-    printf("//%s// \n", tabslay[1][2]);
-    printf("i=%d, j=%d \n", i,count);
+    //printf("i=%d, j=%d \n", i,count);
     lebar = i-1;
-    panjang=((count/lebar)+1);
-    printf("panjang %d \n", panjang);
+    panjang=(count/lebar);
+    //printf("panjang %d \n", panjang);
 
     //memasukkan isi file layout ke array routing
     i=1;
@@ -146,22 +99,19 @@ int main(){
     hitung=0;
     while(file2){
         c = fgetc(file2);
-        printf("masuk\n");
         d=c;
         switch (d){
             case ',':
                 hitung=hitung+1;
-                printf("koma");
+                j=j+1;
                 break;
             case '\n':
                 j=1;
                 i=i+1;
-                printf("enter\n");
                 break;
             case ' ':
-                tabsrout[i][j]=d;
-                printf("%c", tabsrout[i][j]);
-                printf("kosong");
+                tabsrout[i][j]=' ';
+                //printf("%c", tabsrout[i][j]);
                 break;
             case '!':
             case '@':
@@ -174,23 +124,12 @@ int main(){
             case '(':
             case ')':
                 tabsrout[i][j]=d;
-                printf("%c", tabsrout[i][j]);
-                printf("isi");
-                j=j+1;
-            default :
-                printf("/%c/ \n", d);
+                //printf("%c", tabsrout[i][j]);
         }
         if(feof(file2)){
             break;
         }
     }
-
-    //cobacoba
-   /* printf("//%c// \n", tabsrout[1][2]);
-    printf("i=%d, j=%d \n", i,hitung);
-    lebar = i-1;
-    panjang=((hitung/lebar)+1);
-    printf("panjang %d", panjang);*/
 
 
     //Menu utama
@@ -200,7 +139,7 @@ int main(){
     printf("Pilih Mode: ");
     scanf("%d", &mode);
 
-    cekmode(mode);
+    mode = cekmode(mode);
 
     while ((mode<=7)&&(mode>=1)){
         switch(mode){
@@ -208,7 +147,19 @@ int main(){
                 tampilkanlayout(tabslay, panjang, lebar);
                 break;
             case 2 :
+                layout_manual(tabslay,panjang,lebar);
+                break;
+            case 3 :
                 tampilkanrouting(tabsrout, panjang, lebar);
+                break;
+            case 4 :
+                routing_manual(tabsrout,panjang,lebar);
+                break;
+            case 5 :
+                layout_otomatis();
+                break;
+            case 6 :
+                routing_otomatis();
                 break;
             default :
                 printf("ups salah\n");
@@ -219,9 +170,9 @@ int main(){
         printf("\n\t5. Layout Otomatis \n\t6. Routing Otomatis \n\t7. Design Rule Checker \n\t8. Simpan Proyek dan Keluar\n");
         printf("Pilih Mode: ");
         scanf("%d", &mode);
-        cekmode(mode);
+        mode = cekmode(mode);
     }
     fclose(file1);
     fclose(file2);
-    return 0;
+    return ;
 }
